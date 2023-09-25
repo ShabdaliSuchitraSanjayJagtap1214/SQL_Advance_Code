45092f94cbb42378151c15a15b8e673c5c615451
ShabdaliSuchitraSanjayJagtap1214 <140742495+ShabdaliSuchitraSanjayJagtap1214@users.noreply.github.com>
2023-09-25 21:57:02 +0530

    Update Select Command :- Used to Display Records from Table.sql

--- 5. DQL (Data Query Language) Commands :- It Retrieve Data from the DBMS using SQL Queries/Select Command :- Used to Display Records from Table.sql
+++ 5. DQL (Data Query Language) Commands :- It Retrieve Data from the DBMS using SQL Queries/Select Command :- Used to Display Records from Table.sql
@@ -8,3 +8,12 @@ select * from customer;
 
 -- c.display particular columns of that table
 select CustomerID, customerFirstName, CustomerLastName, CustomerAddress, CustomerCity from customer;
+
+-- d.display first 2 rows of that table(In MySQL)
+select * from customer limit 2;
+
+-- e.display first 2 rows of that table(In SQL)
+select top 2 * from customer;
+
+-- f.display 40 percent:also means show the first two(In SQL) 
+select top 40 percent * from customer; 
